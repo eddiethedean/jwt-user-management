94e3afa4fd0fd1013858ef8f6e9d3a1cd4cb49f5
Odo Matthews <odosmatthews@gmail.com>
2026-06-11 10:03:41 -0400

    Fix Alembic chain for add-command migration.
    Point c4e8f1a2b3d0 at 0004_add_roles_to_users instead of the missing
    3b2e2d5f7a1c revision so alembic upgrade head works.

--- user_management_api/alembic/versions/c4e8f1a2b3d0_add_command_to_users.py
+++ user_management_api/alembic/versions/c4e8f1a2b3d0_add_command_to_users.py
@@ -1,7 +1,7 @@
 """add command to users
 
 Revision ID: c4e8f1a2b3d0
-Revises: 3b2e2d5f7a1c
+Revises: 0004_add_roles_to_users
 Create Date: 2026-05-28
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 
 revision = "c4e8f1a2b3d0"
-down_revision = "3b2e2d5f7a1c"
+down_revision = "0004_add_roles_to_users"
 branch_labels = None
 depends_on = None
 
